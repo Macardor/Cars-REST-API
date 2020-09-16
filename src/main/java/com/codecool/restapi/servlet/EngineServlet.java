1f9460f8947acdeb1b3f31f8cf91cb751dd9fdb5
Maciek <macardorpl@gmail.com>
2020-09-16 12:10:41 +0200

    Post, put and delete method in EngineServlet

--- src/main/java/com/codecool/restapi/servlet/EngineServlet.java
+++ src/main/java/com/codecool/restapi/servlet/EngineServlet.java
@@ -1,5 +1,6 @@
 package com.codecool.restapi.servlet;
 
+import com.codecool.restapi.dbconnection.Dao;
 import com.codecool.restapi.model.Engine;
 import com.google.gson.Gson;
 
@@ -8,44 +9,89 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
 
-@WebServlet("/engines")
+@WebServlet(name = "EngineServlet", urlPatterns = {"/engines", "/engines/id"})
 public class EngineServlet extends HttpServlet {
 
     private Gson gson = new Gson();
+    private Dao dao = new Dao();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Engine engine = new Engine(2.2f, 300);
-        engine.setId(1);
-        String engineJsonString = this.gson.toJson(engine);
-        System.out.println(engineJsonString);
+        String url = null;
+        url = (String) request.getAttribute("javax.servlet.forward.request_uri");
+        url = url == null ? request.getRequestURI() : url;
+        List<String> splittedUrl = Arrays.asList(url.split("/"));
 
-//        String engineJsonString = "{\"name\":\"John\", \"age\":31, \"city\":\"New York\"}";
+        String engineJsonString;
+        long id;
+
+        if(splittedUrl.size() == 4){
+            id = Long.parseLong(splittedUrl.get(3));
+            Engine engine = (Engine) dao.get(Engine.class, id);
+            engineJsonString = this.gson.toJson(engine);
+        }else{
+            List<Engine> engines =  dao.getAll(Engine.class);
+            engineJsonString = this.gson.toJson(engines);
+        }
 
         PrintWriter out = response.getWriter();
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         out.print(engineJsonString);
         out.flush();
-        //System.out.println("DO GET");
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        System.out.println("DO POST");
+        StringBuilder stringBuilder = new StringBuilder();
+        String line = null;
+
+        BufferedReader bufferedReader = request.getReader();
+        while ((line = bufferedReader.readLine()) != null){
+            stringBuilder.append(line);
+        }
+
+        String engineJsonString = this.gson.toJson(stringBuilder.toString());
+        Engine engine = this.gson.fromJson(engineJsonString, Engine.class);
+        dao.add(engine);
     }
 
     @Override
     protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        System.out.println("DO PUT");
+        StringBuilder stringBuilder = new StringBuilder();
+        String line = null;
+
+        BufferedReader bufferedReader = request.getReader();
+        while ((line = bufferedReader.readLine()) != null){
+            stringBuilder.append(line);
+        }
+
+        String engineJsonString = this.gson.toJson(stringBuilder.toString());
+        Engine engine = this.gson.fromJson(engineJsonString, Engine.class);
+        dao.update(engine, Engine.class);
     }
 
     @Override
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        System.out.println("DO DELETE");
+        String url = null;
+        url = (String) request.getAttribute("javax.servlet.forward.request_uri");
+        url = url == null ? request.getRequestURI() : url;
+        List<String> splittedUrl = Arrays.asList(url.split("/"));
+
+        long id;
+
+        if(splittedUrl.size() == 4){
+            id = Long.parseLong(splittedUrl.get(3));
+            dao.delete(Engine.class, id);
+        }else{
+            System.out.println("No id provided to delete record");
+        }
     }
 
 }
